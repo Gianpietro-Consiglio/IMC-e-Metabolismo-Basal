22136ee90b2a033b2f1e23a610ae600d39ad7f71
unknown <gianpietro.consiglio13@gmail.com>
2021-08-08 00:08:40 -0300

    1.0

---
+++ novo_teste/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿
+namespace novo_teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F);
+            this.label1.Location = new System.Drawing.Point(101, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(599, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "APLICATIVO DE IMC E TAXA BASAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label2.Location = new System.Drawing.Point(8, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(72, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label3.Location = new System.Drawing.Point(8, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Idade:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(72, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label4.Location = new System.Drawing.Point(8, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Peso:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(72, 135);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(168, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label5.Location = new System.Drawing.Point(8, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Altura:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F);
+            this.label8.Location = new System.Drawing.Point(8, 163);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Sexo:";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(72, 161);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(77, 21);
+            this.radioButton7.TabIndex = 18;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Homem";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(155, 163);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(72, 21);
+            this.radioButton8.TabIndex = 19;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Mulher";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.textBox7);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(368, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(649, 378);
+            this.panel1.TabIndex = 20;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 378);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(12, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 40);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(12, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 40);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Apagar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(12, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 40);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label9.Location = new System.Drawing.Point(371, 42);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 29);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "IMC: ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(440, 48);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(110, 22);
+            this.textBox5.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(479, 123);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(110, 22);
+            this.textBox6.TabIndex = 4;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label10.Location = new System.Drawing.Point(371, 115);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(102, 29);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "BASAL:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(505, 87);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(110, 22);
+            this.textBox7.TabIndex = 6;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label11.Location = new System.Drawing.Point(371, 80);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(134, 29);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Resultado:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label12.Location = new System.Drawing.Point(376, 218);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(84, 29);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Dicas:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(381, 259);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(234, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1029, 470);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ novo_teste/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+namespace novo_teste
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer audio = new SoundPlayer(Properties.Resources.botaoNumerico);
+            if (textBox1.Text == "" | textBox2.Text == "" | textBox3.Text == "" | textBox4.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos!");
+                return;
+            }
+
+            string nome, converter, converter2;
+            int idade;
+            float  peso, altura, altura2, total;
+            double total2;
+                           
+                
+            
+
+            nome = textBox1.Text;
+            idade = int.Parse(textBox2.Text);
+            peso = float.Parse(textBox3.Text);
+            altura = float.Parse(textBox4.Text);
+            total =  peso / (altura * altura);
+            textBox5.Text = Math.Round(total, 1).ToString();
+            
+
+            // IMC
+            if (total < 18.5)
+            {
+                pictureBox1.Image = Properties.Resources.magro;
+                textBox7.Text = "Abaixo do Peso";
+                richTextBox1.Text = nome + ", crie o hábito de fazer musculação para ganhar mais massa magra!";
+
+            }
+
+            else if (total > 18.4 && total < 24.9)
+            {
+                pictureBox1.Image = Properties.Resources.normal;
+                textBox7.Text = "Peso normal";
+                richTextBox1.Text = nome + ", parabéns, continue assim!";
+
+            }
+
+            else if (total > 24.9 && total < 29.9)
+            {
+                pictureBox1.Image = Properties.Resources.sobrepeso;
+                textBox7.Text = "Sobrepeso";
+                richTextBox1.Text = nome + ", não está tão ruim, tente fazer mais atividade física!";
+
+            }
+
+            else if (total > 29.9 && total < 34.9)
+            {
+                pictureBox1.Image = Properties.Resources.obesidade_;
+                textBox7.Text = "Obesidade 1";
+                richTextBox1.Text = nome + ", tente fazer um pouco mais de atividade física!";
+
+            }
+
+            else if (total > 34.9 && total < 39.9)
+            {
+                pictureBox1.Image = Properties.Resources.obesidade_;
+                textBox7.Text = "Obesidade 2";
+                richTextBox1.Text = nome + ", cuidado, tente se alimentar melhor e fazer atividades físicas!";
+
+            }
+
+            else
+            {
+                pictureBox1.Image = Properties.Resources.obesidade_;
+                textBox7.Text = "Obesidade 3";
+                richTextBox1.Text = nome + ", tome cuidado, é necessário fazer atividade física e se alimentar melhor!";
+            }
+
+            converter = altura.ToString();
+            converter2 = peso.ToString();
+            if (converter.Contains(","))
+            {
+                
+
+            }
+
+            else
+            {
+                MessageBox.Show("Utilize a altura com vírgula!");
+                textBox4.Clear();
+                return;
+            }
+
+            if (converter2.Contains(","))
+            {
+
+            }
+
+            else
+            {
+                MessageBox.Show("Utilize o peso com vírgula!");
+                textBox3.Clear();
+                return;
+            }
+
+            
+
+
+
+
+
+            // TAXA BASAL
+            altura2 = altura * 100;
+            if (radioButton7.Checked)
+            {
+                total2 = 66 + (13.7 * peso) + (5 * altura2) - (6.8 * idade);
+                textBox6.Text = Math.Round(total2, 1).ToString();
+            }
+
+           
+            else
+            {
+                total2 = 665 + (9.6 * peso) + (1.8 * altura2) - (4.7 * idade);
+                textBox6.Text = Math.Round(total2, 1).ToString();
+            }
+
+            audio.Play();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            richTextBox1.Clear();
+            pictureBox1.Image = null;
+        }
+    }
+}
